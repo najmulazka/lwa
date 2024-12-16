11e75f29f9274b03891f127d7205263a520d89db
najmulazka <najmulazka225@gmail.com>
2024-12-16 15:55:03 +0700

    integration recommend be to fe

--- fe/src/components/pages/dreamJobUser.jsx
+++ fe/src/components/pages/dreamJobUser.jsx
@@ -5,11 +5,13 @@ import { useNavigate } from 'react-router-dom';
 import { getProfessions, predict } from '../../services/profession.service';
 import { toast } from 'react-toastify';
 import ModalPopUp from '../elements/ModalPopUp';
+import { createSelfCheckProfession } from '../../services/selfCheckProfession.service';
 
 function DreamJobUser() {
   const [professions, setProfessions] = useState([]);
   const [selectedCareer, setSelectedCareer] = useState('default');
   const [customCareer, setCustomCareer] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [prediction, setPrediction] = useState('');
   console.log(prediction);
   const [isOpen, setIsOpen] = useState(false);
@@ -30,28 +32,89 @@ function DreamJobUser() {
   }, [navigate]);
 
   const handleSubmit = async () => {
-    const careerToSend = selectedCareer === 'other' && customCareer.trim() !== '' ? customCareer : selectedCareer;
+    try {
+      setIsLoading(true);
+      const careerToSend = selectedCareer === 'other' && customCareer.trim() !== '' ? customCareer : selectedCareer;
 
-    if (careerToSend === 'default' || careerToSend === 'other' || careerToSend === '') {
-      toast.error('Please select or input a valid career!', { position: 'top-center' });
-    } else {
-      const text = { text: careerToSend };
-      const data = await predict(text);
-      console.log(careerToSend);
-      console.log(data);
-      setPrediction(data);
-      toggleModal();
+      if (careerToSend === 'default' || careerToSend === 'other' || careerToSend === '') {
+        toast.error('Please select or input a valid career!', { position: 'top-center' });
+      } else {
+        if (selectedCareer === 'other') {
+          const text = { text: careerToSend };
+          const data = await predict(text);
+          console.log(careerToSend);
+          console.log(data);
+          setPrediction(data);
+          toggleModal();
+        }
+
+        if (selectedCareer !== 'other' && selectedCareer !== 'default') {
+          const data = {
+            profession: selectedCareer,
+          };
+          await createSelfCheckProfession(data);
+          navigate('/user');
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleNo = async () => {
+    try {
+      setIsLoading(true);
+      const data = {
+        profession: customCareer,
+      };
+      await createSelfCheckProfession(data);
+      navigate('/user');
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleYes = async () => {
+    try {
+      setIsLoading(true);
+      const data = {
+        profession: prediction,
+      };
+      await createSelfCheckProfession(data);
+      navigate('/user');
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row h-screen">
-      {isOpen && prediction !== '' && (
-        <ModalPopUp isOpen={isOpen} toggleModal={toggleModal}>
+      {isOpen && prediction !== '' && customCareer !== '' && (
+        <ModalPopUp isOpen={isOpen} toggleModal={toggleModal} className="">
           Do You Mean {prediction}?
+          <div className="flex flex-row mt-10 justify-center space-x-10">
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <>
+                <button className="border border-red-500 px-6 py-2 rounded-md bg-red-500 text-white hover:bg-white hover:text-red-500" onClick={handleNo}>
+                  No
+                </button>
+                <button className="border border-green-500 px-6 py-2 rounded-md bg-green-500 text-white hover:bg-white hover:text-green-500" onClick={handleYes}>
+                  Yes
+                </button>
+              </>
+            )}
+          </div>
         </ModalPopUp>
       )}
       <div className="flex flex-col px-10 md:px-24 pt-5 md:pt-10 md:w-3/5 h-screen" style={{ backgroundImage: `url(${vectorDreamJob})`, backgroundSize: '25%', backgroundRepeat: 'no-repeat', backgroundPosition: 'left bottom' }}>
@@ -82,7 +145,7 @@ function DreamJobUser() {
         </div>
 
         <button className="text-xs md:text-base font-semibold py-1 md:w-1/3 px-2 md:py-2 md:px-4 border border-2 border-black rounded-lg bg-black text-white hover:bg-white hover:text-black" type="button" onClick={handleSubmit}>
-          <span>Let&#39;s Get Started!</span>
+          <span>{isLoading ? 'Loading...' : `Let's Get Started!`}</span>
         </button>
 
         <div className="md:fixed bottom-10 text-semibold absolute">
--- fe/src/services/selfCheckProfession.service.js
+++ fe/src/services/selfCheckProfession.service.js
@@ -3,6 +3,29 @@ import { CookiesKey, CookiesStorage } from '../utils/cookies';
 
 const BASE_URL = import.meta.env.VITE_URL;
 
+export const createSelfCheckProfession = async (data) => {
+  const token = CookiesStorage.get(CookiesKey.AuthToken);
+
+  if (!token) {
+    throw new Error('Unauthorized: Token is missing');
+  }
+
+  try {
+    const response = await axios.post(`${BASE_URL}/self-check-profession`, data, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    return response.data.data;
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      CookiesStorage.remove(CookiesKey.AuthToken);
+      throw new Error('Unauthorized: Token is invalid');
+    }
+    throw error;
+  }
+};
+
 export const getSelfCheckProfessions = async () => {
   const token = CookiesStorage.get(CookiesKey.AuthToken);
 
