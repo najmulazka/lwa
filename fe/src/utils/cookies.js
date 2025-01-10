9bd973191c283b82a52235d72b7a208fd66295f0
najmulazka <najmulazka225@gmail.com>
2025-01-10 18:12:31 +0700

    solv set cookies

--- fe/src/utils/cookies.js
+++ fe/src/utils/cookies.js
@@ -11,7 +11,7 @@ export const CookiesKey = {
 
 const CookiesOptions = {
   path: '/',
-  // secure: process.env.NODE_ENV === 'production',
+  secure: import.meta.env.NODE_ENV === 'production',
   maxAge: 24 * 60 * 60,
 };
 
