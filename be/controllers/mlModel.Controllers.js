1a743da50857035e14bd1c08066992ca0790e81c
najmulazka <najmulazka225@gmail.com>
2025-01-11 12:40:29 +0700

    solv dream job to buntu

--- be/controllers/mlModel.Controllers.js
+++ be/controllers/mlModel.Controllers.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 // Fungsi untuk menjalankan Python
 const runPython = (text) => {
   return new Promise((resolve, reject) => {
-    const python = spawn('python3', ['./python/ml-model.py', text]);
+    const python = spawn('python3', ['../python/ml-model.py', text]);
 
     let result = '';
     python.stdout.on('data', (data) => {
@@ -43,6 +43,8 @@ module.exports = {
 
       res.status(200).json({ status: true, message: 'Ok', data: Object.keys(data).pop() });
     } catch (error) {
+      console.log(error.message);
+      console.log(error);
       res.status(500).json({ error: error });
     }
   },
